a0f0fb9177e973a871b3913ab410a183ea74c05d
Vladyslav Halushkin <v.halushkin@gmail.com>
2019-02-23 17:18:45 +0200

    build released

---
+++ build/precache-manifest.26a65fa4432c35d0db00fe747a88d115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d95aa6e00ff327933710",
+    "url": "/static/js/main.d95aa6e0.chunk.js"
+  },
+  {
+    "revision": "f0f997e26ef640ee6d34",
+    "url": "/static/js/2.f0f997e2.chunk.js"
+  },
+  {
+    "revision": "d95aa6e00ff327933710",
+    "url": "/static/css/main.89b964cc.chunk.css"
+  },
+  {
+    "revision": "d7156e73e601a85c4974c9bee454d745",
+    "url": "/index.html"
+  }
+];
